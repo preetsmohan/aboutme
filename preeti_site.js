e15c69187863d43bba8442b0469ede43032caa3a
eleventhirtyeight <guitar.gamer.geek@gmail.com>
2015-06-21 01:43:03 -0400

    navbar is nice with links

--- preeti_site.js
+++ preeti_site.js
@@ -19,13 +19,12 @@ window.onload = function(){
 
 $(window).scroll(function () {
     if( $(window).scrollTop() > $('.filler1').offset().top && !($('.introtext').hasClass('nav'))){
-      $('.introtext').addClass('nav');
 	  $('.navbar').slideDown(100);
 	  
     } else if ($(window).scrollTop() == 0){
-      $('.introtext').removeClass('nav');
 	  $('.navbar').slideUp(100);
     }
+	/*
 	 if( $(window).scrollTop() > $('.filler2').offset().top){
 	  $('#div1').slideDown(100);
 	  
@@ -44,4 +43,13 @@ $(window).scroll(function () {
     } else if ($(window).scrollTop() < $('.filler4').offset().top){
 	  $('#div3').slideUp(100);
     }
+	if( $(window).scrollTop() > $('.filler4').offset().top){
+	  $('#div4').slideDown(100);
+	   $('#div5').slideDown(100);
+	  
+    } else if ($(window).scrollTop() < $('.filler4').offset().top){
+	  $('#div4').slideUp(100);
+	  $('#div5').slideUp(100);
+    }
+	 */
 });
